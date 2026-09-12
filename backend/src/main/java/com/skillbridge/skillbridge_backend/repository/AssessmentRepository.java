ada5f4d5b480a2167a4f687fce0ae1f3ed56c889
Dakshkawatra <kawatradaksh1@gmail.com>
2026-09-12 23:50:06 +0530

    Delete backend/src/main/java/com/skillbridge/skillbridge_backend/repository/AssessmentRepository.java

--- backend/src/main/java/com/skillbridge/skillbridge_backend/repository/AssessmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.skillbridge.skillbridge_backend.repository;
-
-import com.skillbridge.skillbridge_backend.entity.Assessment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssessmentRepository extends JpaRepository<Assessment, Long> {
-}
